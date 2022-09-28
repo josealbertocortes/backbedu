ecc7b85808500df4c9d18db0a5c4740f036159b0
josealbertocortes <josealbertocortesayala94@gmail.com>
2022-09-28 11:22:25 -0500

    se realiza la conexion con la base de datos

---
+++ app.js
@@ -0,0 +1,21 @@
+const dotenv = require('dotenv').config();
+const express = require('express');
+const sequelize = require('./config/db');
+
+
+const app = express();
+app.use(express.json());
+
+
+try {
+    sequelize.authenticate();
+    sequelize.sync();
+    console.log('Connected to DB');
+} catch (error) {
+    console.log('Unable to connect to DB:', error);
+}
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log("Server listing on PORT 3000");
+});
---
+++ config/db.js
@@ -0,0 +1,18 @@
+const { Sequelize } = require('sequelize');
+
+console.log()
+  const sequelize = new Sequelize({
+    database: process.env.DATABASE,
+    username: process.env.USERNAMEDATA,
+    password: process.env.PASSWORD,
+    host: process.env.HOST,
+    port: 5432,
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true, // This will help you. But you will see nwe error
+        rejectUnauthorized: false // This line will fix new error
+      }
+    },
+  });
+module.exports = sequelize;
