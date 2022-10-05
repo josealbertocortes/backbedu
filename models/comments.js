18446442403e32a4132e5f569eecd11043cc0fa8
MarioEduardo <eduardoat44@gmail.com>
2022-10-05 13:40:26 -0500

    Mario_models were created

--- models/comments.js
+++ models/comments.js
@@ -2,14 +2,6 @@ const { Sequelize, DataTypes } = require('sequelize');
 const sequelize = require('../config/db');
 
 const Comment = sequelize.define('Comment', {
-    idUser: {
-        type: DataTypes.VARCHAR(50),
-        allowNull: false
-    },
-    idMovie: {
-        type: DataTypes.VARCHAR(50),
-        allowNull: false
-    },
     comment:{
         type: DataTypes.TEXT,
         allowNull: false,
